c32aeabab67521204b0f053d6810aa3616f8658b
KyleHaggin <KyleHaggin@users.noreply.github.com>
2019-10-11 09:14:05 -0700

    Finish Part2

--- Code Challenge/northwind.py
+++ Code Challenge/northwind.py
@@ -2,4 +2,16 @@
 import sqlite3
 
 # Connect to database
-pathSQL = northwind_small.sqlite3
+pathSQL = 'northwind_small.sqlite3'
+conn = sqlite3.connect(pathSQL)
+curs = conn.cursor()
+
+# write code to answer questions
+print(curs.execute(
+    'SELECT * FROM product ORDER BY UnitPrice DESC LIMIT 10'
+).fetchall())
+
+averageAge = curs.execute(
+    'SELECT AVG(HireDate - BirthDate) FROM Employee'
+).fetchall()
+print(averageAge)
